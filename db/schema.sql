d26f52394babc695ef8910d170ba3a7f5c79b9cc
MarcusAdams1220 <marcusadams1220@gmail.com>
2022-06-06 17:57:07 +1000

    fixed inital bugs: users can sign up & login

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,12 @@
 CREATE DATABASE jobs_db;
 \c jobs_db
 
-DROP TABLE jobs;
-
 CREATE TABLE jobs(
   id SERIAL PRIMARY KEY,
   user_id INTEGER,
   position_title TEXT,
   job_url TEXT,
-  closing_date DATETIME,
+  closing_date TEXT,
   company_name TEXT,
   company_url TEXT,
   contact_person TEXT,
@@ -17,18 +15,12 @@ CREATE TABLE jobs(
   salary TEXT,
   notes_about_company TEXT,
   job_advertisement_screen_shot TEXT,
-  date_applied DATETIME,
+  date_applied TEXT,
   upcoming_interview BOOLEAN,
   completed_interview BOOLEAN,
   received_job_offer BOOLEAN
   );
 
-
--- INSERT INTO jobs(name, clue, address) VALUES
--- ('Lost coins', 'Marcus'' favourite hiding place', 'some park near Marcus'' house'),
--- ('Kenny''s duck', 'northwest most corner of a pond near Kenny''s house', 'Pond near Kenny''s house');
-
-SELECT * FROM jobs;
 
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
@@ -36,4 +28,11 @@ CREATE TABLE users(
   email TEXT,
   password_digest TEXT
   );
+
+
+
+-- DON'T COPY THIS!
+SELECT * FROM jobs;
+DROP TABLE jobs;
+DROP TABLE users;
 
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require("express");
 const logger = require('./middlewares/logger')
 const sessions = require('./middlewares/sessions')
 
-const jobController = require('./controllers/jobs_controller')
+const jobsController = require('./controllers/jobs_controller')
 const usersController = require('./controllers/users_controller')
 const sessionsController = require('./controllers/sessions_controller')
 
 const app = express();
-const port = process.evn.Port || 2222;
+const port = process.env.Port || 2222;
 
 app.listen(port, () => console.log(`listening to port ${port}`));
 
