196394dbd2390d5d304e772717edf4c457509167
BSChampion <breevaneerden@outlook.com>
2022-06-08 14:44:38 +1000

    email not found

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -10,19 +10,15 @@ router.post("/", (req, res) => {
 
   User
     .findByEmail(email)
-    .then(user => {
-      if (user === undefined) {
-        console.log("user not found")
-      }
-    })
     .then((user) => {
+    if (user === undefined) {console.log("user not found")} else if (user) {
     const isValidPassword = bcrypt.compareSync(password, user.password_digest)
     if (user && isValidPassword) {
       req.session.userId = user.id;
       res.json({userName: user.name, sessionId: req.session.userId})
-    } else {console.log("password incorrect")}
+    }
+    }
   })
 })
-
 
 module.exports = router
