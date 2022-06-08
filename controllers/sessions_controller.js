56fbe5327271710eecb9fc44e88fe5748709669c
BSChampion <breevaneerden@outlook.com>
2022-06-08 11:04:11 +1000

    login fails

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -10,53 +10,19 @@ router.post("/", (req, res) => {
 
   User
     .findByEmail(email)
+    .then(user => {
+      if (user === undefined) {
+        console.log("user not found")
+      }
+    })
     .then((user) => {
     const isValidPassword = bcrypt.compareSync(password, user.password_digest)
     if (user && isValidPassword) {
       req.session.userId = user.id;
       res.json({userName: user.name, sessionId: req.session.userId})
-    }
+    } else {console.log("password incorrect")}
   })
 })
 
-// router.post("/", (req, res) => {
-//   const { email, password } = req.body
-  
-//   User
-//     .findByEmail(email)
-//     .then(user => {
-//       if (user === undefined) {
-//         console.log("user not found")
-//       }
-//     })
-//     // if user true
-//     .then(user => {
-//       const isValidPassword = (bcrypt.compareSync(password, user.password_digest))
-//       if (user && isValidPassword) {
-//         req.session.userId = user.id
-//         res.json({ userName: user.name })
-//       }
-//     })
-// })
-
-// // Login Session controller
-// router.post('/', (req, res) => {
-//     User.findByEmail(req.body.email)
-//         .then(user => {
-//             if (bcrypt.compareSync(req.body.password, user.password_digest)) {
-//                 req.session.userId = user.id
-//                 res.json({ "message": "Login Successful" })
-//             } else {
-//                 res.status(401).json({ error: "No such user found" })
-//             }
-//         })
-// })
-
-// // Logout Session controller
-// router.delete('/', (req, res) => {
-//     req.session.destroy()
-//     res.json({})
-//     window.location = "/"
-// })
 
 module.exports = router
