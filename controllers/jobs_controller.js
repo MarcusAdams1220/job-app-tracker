8a3002f31ecce2e20dcc450398b67f605908802f
ciaociaoworld <yellowzung@gmail.com>
2022-06-09 09:46:37 +1000

    edit

--- controllers/jobs_controller.js
+++ controllers/jobs_controller.js
@@ -27,6 +27,7 @@ router.post("/:id", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
+<<<<<<< HEAD
   const id = req.params.id
   const {position, jobAdURL, closingDate, companyName, companyURL, contactPerson, contactPhone, contactEmail, typeOfEmployment, salary, notesAboutCompany, jobAdScreenshot, dateApplied, upcomingInterview, completedInterview, receivedJob} = req.body
 
@@ -34,6 +35,13 @@ router.put("/:id", (req, res) => {
     .update(id, position, jobAdURL, closingDate, companyName, companyURL, contactPerson, contactPhone, contactEmail, typeOfEmployment, salary, notesAboutCompany, jobAdScreenshot, dateApplied, upcomingInterview, completedInterview, receivedJob)
     .then(console.log(`${salary}`))
     .then(console.log(`${id}`))
+=======
+  const {id, userId, position, jobAdURL, closingDate, companyName, companyURL, contactPerson, contactPhone, contactEmail, typeOfEmployment, salary, notesAboutCompany, jobAdScreenshot, dateApplied, upcomingInterview, completedInterview, receivedJob,} = req.body
+
+  Job
+    .update(id, userId, position, jobAdURL, closingDate, companyName, companyURL, contactPerson, contactPhone, contactEmail, typeOfEmployment, salary, notesAboutCompany, jobAdScreenshot, dateApplied, upcomingInterview, completedInterview, receivedJob)
+    .then(console.log(salary))
+>>>>>>> 99f5843 (change)
     .then(job => res.json(job));
 })
 
@@ -46,3 +54,8 @@ router.delete("/:id", (req, res) => {
 })
 
 module.exports = router;
+
+
+function renderEditJob() {
+  document.querySelector('#').innerHTML = 
+}
