088fda70a7b5e161fe8c3760e605a3b790c61e3b
ciaociaoworld <yellowzung@gmail.com>
2022-06-08 13:38:01 +1000

    dwdw

--- client/js/components/job_list.js
+++ client/js/components/job_list.js
@@ -25,7 +25,7 @@ function renderJobs() {
       <h2>${job.position_title}</h2>
       <span onClick="deleteJob(event)" 
       >Delete</span>
-      <span onClick="updateJob(event)">Edit</span>
+      <span onClick="renderEditJob(event)">Edit</span>
     </header>
     <p>Job Type: ${job.type_of_employment}</p>
     <p>Salary: ${job.salary}</p>
@@ -47,40 +47,49 @@ function renderJobs() {
 }
 
 function renderEditJob(event) {
-  event.preventDefault()
-  const jobDOM = updateBtn.closest('.job')
-  document.querySelector(jobDOM).innerHTML = 
+  const jobDOM = event.target.closest('.job')
+  document.querySelector('').innerHTML = 
   `
     <section class="job" data-id="${jobDOM.dataset.id}">
-      <header>
-        <h2>${job.position_title}</h2>
-      </header>
-      <p>Job Type: ${job.type_of_employment}</p>
-      <p>Salary: ${job.salary}</p>
-      <p>Job Link: ${job.job_url}</p>
-      <p>Closing Date: ${job.closing_date}</p>
-      <p>Company Name: ${job.company_name}</p>
-      <p>Company Website: ${job.company_url}</p>
-      <p>Contact Person: ${job.contact_person}</p>
-      <p>Contact Phone Number: ${job.contact_phone}</p>
-      <p>Contact Email: ${job.contact_email}</p>
-      <p>Notes: ${job.notes_about_company}</p>
-      <p>Job Ad Screenshot: ${job.job_advertisement_screen_shot}</p>
-      <p>Date Applied: ${job.date_applied}</p>
-      <p>Upcoming Interview: ${upcomingOrNot(job.upcoming_interview)}</p>
-      <p>Completed Interview: ${completedOrNot(job.completed_interview)}</p>
-      <p>Received Job Offer: ${receivedOrNot(job.received_job_offer)}</p>
+      <form onSubmit="updateJobs(event)">
+        <header>
+          <input type="text" name="position" placeholder=${jobDOM.dataset.position_title}>
+        </header>
+
+        <p>Job Type: </p> <input type="text" name="typeOfEmployment" placeholder=${jobDOM.dataset.type_of_employment}>
+
+        <p>Salary: </p> <input type="text" name="salary" placeholder=${jobDOM.dataset.salary}> 
+
+        <p>Job Link: </p> <input type="text" name="jobAdURL" placeholder=${jobDOM.dataset.job_url}> 
+
+        <p>Closing Date: </p> <input type="text" name="closingDate" placeholder="${jobDOM.dataset.closing_date}">
+
+        <p>Company Name: </p> <input type="text" name="CompanyName" placeholder="${jobDOM.dataset.company_name}">
+
+        <p>Company Website: </p> <input type="text" name="CompanyName" placeholder="${jobDOM.dataset.company_url}">
+
+        <p>Contact Person: </p> <input type="text" name="contactPerson" placeholder="${jobDOM.dataset.contact_person}">
+
+        <p>Contact Phone Number: </p> <input type="text" name="contactPhone" placeholder="${jobDOM.dataset.contact_phone}">
+
+        <p>Contact Email: </p> <input type="text" name="contactEmail" placeholder="${jobDOM.dataset.contact_email}">
+
+        <p>Notes: </p> <input type="text" name="notesAboutCompany" placeholder="${jobDOM.dataset.notes_about_company}">
+
+        <p>Job Ad Screenshot: </p> <input type="text" name="notesAboutCompany" placeholder="${jobDOM.dataset.notes_about_company}">
+
+        <p>Date Applied: <input type="text" name="dateApplied" placeholder="${jobDOM.dataset.date_applied}">
+
+        <button>Done</button>
+      </form>
     </section>
-    `).join('')
+    `
 }
 
 function updateJobs(event) {
   event.preventDefault()
- 
-  const updateBtn = event.target
-  const jobDOM = updateBtn.closest('.job')
-  const jobId = jobDOM.dataset.id
-
+  const form  = event.target
+  const jobId = form.dataset.id
   const data = Object.fromEntries(new FormData(form))
 
   fetch(`/api/jobs/${jobId}`, {
@@ -104,9 +113,9 @@ function updateJobs(event) {
     currentJob.notes_about_company = job.notes_about_company
     currentJob.job_advertisement_screen_shot = job.job_advertisement_screen_shot
     currentJob.date_applied = job.date_applied
-    currentJob.upcoming_interview = job.upcoming_interview
-    currentJob.completed_interview = job.completed_interview
-    currentJob.received_job_offer = job.received_job_offer
+    // currentJob.upcoming_interview = job.upcoming_interview
+    // currentJob.completed_interview = job.completed_interview
+    // currentJob.received_job_offer = job.received_job_offer
 
     renderJobList()
   })
@@ -116,6 +125,7 @@ function updateJobs(event) {
 function deleteJob(event) {
   const deleteBtn = event.target
   const jobDOM = deleteBtn.closest('.job')
+  console.log(`${jobDOM}`)
   const jobId = jobDOM.dataset.id
   fetch(`/api/jobs/${jobId}`, {
     method: 'DELETE',
