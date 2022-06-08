2fc213f96444681a9b948c1a5024de9e1d934bd8
ciaociaoworld <yellowzung@gmail.com>
2022-06-08 11:03:12 +1000

    change

--- models/job.js
+++ models/job.js
@@ -69,7 +69,7 @@ const Job = {
     received_job_offer = $17 WHERE id = $18`
     
     return db.query(sql, [userId, position, jobAdURL, closingDate, companyName, companyURL, contactPerson, contactPhoneNumber, contactEmail, typeOfEmployment, salary, notesAboutCompany, jobAdScreenshot, dateApplied, upcomingInterview, completedInterview, receivedJob, id])
-    .then(dbRes => dbRes.rows[0])
+    .then(dbRes => dbRes.rows)
   },
   
 
