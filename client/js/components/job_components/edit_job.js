4e1ce8df69ca2c8ec3b01ed8b0f2b79bc383d5e1
BSChampion <breevaneerden@outlook.com>
2022-06-09 12:49:19 +1000

    nav for edit job function

--- client/js/components/job_components/edit_job.js
+++ client/js/components/job_components/edit_job.js
@@ -1,72 +1,3 @@
-function renderEditJob(event) {     
-  const editBtn = event.target
-  const jobDOM = editBtn.closest('.job')
-  const jobId = jobDOM.dataset.id
-  let currentJob = state.jobs.filter(t => t.id == jobId)[0]
-
-  document.querySelector('#page').innerHTML = 
-  `
-    <section class="job" data-id="${currentJob.id}">
-      <form onSubmit="updateJobs(event)">
-        <header>
-          <p>Position: </p>
-          <input type="text" name="position" value=${currentJob.position_title}>
-        </header>
-
-        <p>Job Type: </p> <input type="text" name="typeOfEmployment" value=${currentJob.type_of_employment}>
-
-        <p>Salary: </p> <input type="text" name="salary" value=${currentJob.salary}> 
-
-        <p>Job Link: </p> <input type="text" name="jobAdURL" value=${currentJob.job_url}> 
-
-        <p>Closing Date: </p> <input type="text" name="closingDate" value="${currentJob.closing_date}">
-
-        <p>Company Name: </p> <input type="text" name="companyName" value="${currentJob.company_name}">
-
-        <p>Company Website: </p> <input type="text" name="companyURL" value="${currentJob.company_url}">
-
-        <p>Contact Person: </p> <input type="text" name="contactPerson" value="${currentJob.contact_person}">
-
-        <p>Contact Phone Number: </p> <input type="text" name="contactPhone" value="${currentJob.contact_phone}">
-
-        <p>Contact Email: </p> <input type="text" name="contactEmail" value="${currentJob.contact_email}">
-
-        <p>Notes: </p> <input type="text" name="notesAboutCompany" value="${currentJob.notes_about_company}">
-
-        <p>Job Ad Screenshot: </p> <input type="text" name="jobAdScreenshot" value="${currentJob.job_advertisement_screen_shot}">
-
-        <p>Date Applied: <input type="text" name="dateApplied" value="${currentJob.date_applied}">
-
-        <fieldset>
-          <label for="">Upcoming Interview:</label>
-          <select name="upcomingInterview">
-            <option value="no">No</option>
-            <option value="yes">Yes</option>
-          </select>
-        </fieldset>
-
-        <fieldset>
-          <label for="">Completed Interview:</label>
-          <select name="completedInterview">
-            <option value="no">No</option>
-            <option value="yes">Yes</option>
-          </select>
-        </fieldset>
-
-        <fieldset>
-          <label for="">Received Job Offer:</label>
-          <select name="receivedJobOffer">
-            <option value="no">No</option>
-            <option value="yes">Yes</option>
-          </select>
-        </fieldset>
-
-        <button>Done</button>
-      </form>
-    </section>
-    `
-}
-
 function updateJobs(event) {
   event.preventDefault()
   const form  = event.target
@@ -100,5 +31,14 @@ function updateJobs(event) {
     currentJob.received_job_offer = data.receivedJobOffer
     renderJobList()
   })
+}
+
+function renderEditJob(event) {     
+  const editBtn = event.target
+  const jobDOM = editBtn.closest('.job')
+  const jobId = jobDOM.dataset.id
+  let currentJob = state.jobs.filter(t => t.id == jobId)[0]
+
+  renderJobById(currentJob)
 }
 
